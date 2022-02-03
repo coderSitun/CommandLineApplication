0f8f4b7fb1845d9eb667e46663879ef8e9632b55
Situn <ksaswat007@gmail.com>
2022-02-03 18:56:05 +0000

    Added switch statement to App::execute()

--- src/app.cpp
+++ src/app.cpp
@@ -18,7 +18,14 @@ void App::execute(){
     while(this->appRun){
         while(!commands.empty()){
             std::vector<std::string> parts = parse(commands.front());
-
+            switch(getCommandType(parts[0]))
+            {
+                case ADD:
+                case SUBTRACT:
+                case MULTIPLY:
+                case DIVIDE:
+                default:
+            }
             commands.pop();
         }
         std::this_thread::sleep_for(std::chrono::microseconds(10));
